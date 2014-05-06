2e143ea8f60189c701660731778ca9b4747cb17a
Isa Herico <miss.herico@gmail.com>
2014-05-06 16:37:25 -0700

    first commit, 3 examples in with data binding

---
+++ maincontroller.js
@@ -0,0 +1,55 @@
+app.controller("MainController", function($scope) {
+
+	$scope.selectedPerson = 0;
+	$scope.selectedGenre = null;
+	$scope.people = [
+		{
+			id: 0,
+			name: 'Leon',
+			music: [
+				'Rock',
+				'Metal',
+				'Dubstep',
+				'Electro'
+			]
+		},
+		{
+			id: 1,
+			name: 'Chris',
+			music: [
+				'Indie',
+				'Drumstep',
+				'Dubstep',
+				'Electro'
+			]
+		},
+		{
+			id: 2,
+			name: 'Harry',
+			music: [
+				'Rock',
+				'Metal',
+				'Thrash Metal',
+				'Heavy Metal'
+			]
+		},
+		{
+			id: 3,
+			name: 'Allyce',
+			music: [
+				'Pop',
+				'RnB',
+				'Hip Hop'
+			]
+		}
+	];
+
+
+	// seeing real-time changes on web page
+	$scope.inputValue = "";
+
+	// data binding like a method
+	$scope.understand = "I now understand how $scope works. not really.";
+
+
+});
